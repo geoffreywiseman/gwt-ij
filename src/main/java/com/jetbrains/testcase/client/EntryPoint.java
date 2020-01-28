a8b22111c5a3e5274908590de57ff8902babc852
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2020-01-28 14:20:49 -0500

    Fixing Missing Brace
    Signed-off-by: Geoffrey Wiseman <geoffrey.wiseman@codiform.com>

--- src/main/java/com/jetbrains/testcase/client/EntryPoint.java
+++ src/main/java/com/jetbrains/testcase/client/EntryPoint.java
@@ -2,7 +2,7 @@ package com.jetbrains.testcase.client;
 
 import com.google.gwt.user.client.Window;
 
-public class EntryPoint implements com.google.gwt.core.client.EntryPoint
+public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
 	public void onModuleLoad() {
 		Window.alert( "Hello, World!" );
 	}
