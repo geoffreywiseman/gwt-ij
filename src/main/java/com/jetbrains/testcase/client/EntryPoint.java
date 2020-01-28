9726d9ea1984d1b4da2bd4f1416086efe1e7133f
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2020-01-28 14:07:52 -0500

    Initial commit of test case.

---
+++ src/main/java/com/jetbrains/testcase/client/EntryPoint.java
@@ -0,0 +1,9 @@
+package com.jetbrains.testcase.client;
+
+import com.google.gwt.user.client.Window;
+
+public class EntryPoint implements com.google.gwt.core.client.EntryPoint
+	public void onModuleLoad() {
+		Window.alert( "Hello, World!" );
+	}
+}
